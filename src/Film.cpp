fa00d98c8e474c20e3c1473e63def70c92a1686c
Matei <msungureanu@gmail.com>
2024-03-24 20:05:33 +0200

    test unitar fix

---
+++ src/Film.cpp
@@ -0,0 +1,65 @@
+#include "Film.h"
+
+#include <utility>
+
+Film::Film(std::string numeFilm, float rating)
+{
+	this->numeFilm = std::move(numeFilm);
+	this->rating = rating;
+}
+Film::Film()
+{
+	this->numeFilm = "";
+	this->rating = 0;
+}
+void
+Film::setRating(float rating)
+{
+	this->rating = rating;
+}
+void
+Film::setNumeFilm(std::string numeFilm)
+{
+	this->numeFilm = std::move(numeFilm);
+}
+float
+Film::getRating() const
+{
+	return rating;
+}
+std::string
+Film::getNumeFilm() const
+{
+	return numeFilm;
+}
+bool
+Film::operator==(const Film &film)
+{
+	if (this->numeFilm == film.numeFilm and this->rating == film.rating)
+		return true;
+	else
+		return false;
+}
+Film &
+Film::operator=(const Film &aux)
+{
+	if (this == &aux)
+	{
+		return *this;
+	}
+	this->numeFilm = aux.numeFilm;
+	this->rating = aux.rating;
+	return *this;
+}
+std::ostream &
+operator<<(std::ostream &out, const Film &film)
+{
+	out << "\nnume film: " << film.numeFilm;
+	out << "\nrating: " << film.rating;
+	return out;
+}
+bool
+Film::comparaFilme(const Film &a, const Film &b)
+{
+	return a.numeFilm < b.numeFilm;
+}
